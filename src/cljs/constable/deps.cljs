1ac8dcf884826a7cb9967e7977c2d8ccf16857ea
Sebastian Bensusan <sbensu@gmail.com>
2015-09-30 15:44:34 +0200

    Add different platforms

--- src/cljs/constable/deps.cljs
+++ src/cljs/constable/deps.cljs
@@ -10,7 +10,6 @@
 (def fs (js/require "fs"))
 
 (defn file->folder [p]
-  (println p)
   (path.dirname p))
 
 (defn list-files [dir]
@@ -19,21 +18,30 @@
 (defn join-paths [& args]
   (apply path.join args))
 
-(defn depgraph [srcs]
-  {:pre [(seq? srcs)]}
-  (let [exts (update find/clj :extensions (partial cons ".cljs"))
-        source-fs (apply set/union
-                    (map #(find/find-sources-in-dir % exts) srcs))
-        tracker (file/add-files {} source-fs)
-        dep-graph (tracker ::track/deps)
-        ns-names (set (map (comp second file/read-file-ns-decl) source-fs))
-        part-of-project? (partial contains? ns-names)
-        nodes (filter part-of-project? (reverse (dep/topo-sort dep-graph)))
-        edges (->> nodes
-                (mapcat #(->> (filter part-of-project?
-                                (dep/immediate-dependencies dep-graph %))
-                           (map (partial vector %)))))
-        idx (into {} (map-indexed (fn [i n] [n i]) nodes))
-        json-nodes (mapv (fn [[n i]] {:name (str n)}) (sort-by second idx))
-        json-edges (mapv (fn [[from to]] {:source from :target to}) edges)]
-    {:edges json-edges :nodes json-nodes}))
+(defn platform->ext [platform]
+  {:pre [(keyword platform)]}
+  (get {:clj find/clj
+        :cljs find/cljs
+        :cljc (update find/clj :extensions (partial cons ".cljs"))}
+    platform))
+
+(defn depgraph
+  ([srcs] (depgraph :cljs srcs))
+  ([platform srcs]
+   {:pre [(seq? srcs)]}
+   (let [exts (platform->ext platform) 
+         source-fs (apply set/union
+                     (map #(find/find-sources-in-dir % exts) srcs))
+         tracker (file/add-files {} source-fs)
+         dep-graph (tracker ::track/deps)
+         ns-names (set (map (comp second file/read-file-ns-decl) source-fs))
+         part-of-project? (partial contains? ns-names)
+         nodes (filter part-of-project? (reverse (dep/topo-sort dep-graph)))
+         edges (->> nodes
+                 (mapcat #(->> (filter part-of-project?
+                                 (dep/immediate-dependencies dep-graph %))
+                            (map (partial vector %)))))
+         idx (into {} (map-indexed (fn [i n] [n i]) nodes))
+         json-nodes (mapv (fn [[n i]] {:name (str n)}) (sort-by second idx))
+         json-edges (mapv (fn [[from to]] {:source from :target to}) edges)]
+     {:edges json-edges :nodes json-nodes})))
